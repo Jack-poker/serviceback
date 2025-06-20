3449de734862f1f0ee2d9129ba0fe6fc468fe982
millbox5 <iradukundajosiane225@gmail.com>
2025-06-20 11:46:25 +0000

    ok

--- users.py
+++ users.py
@@ -113,7 +113,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("CORS_ORIGINS", "http://localhost:8080,http://localhost:8001").split(","),
+    allow_origins=os.getenv("CORS_ORIGINS", "http://localhost:8080,http://localhost:8001,http://192.168.1.149:8080").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
